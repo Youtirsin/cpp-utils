a9ba46b38da7c0dbc111eb681044f7e6868cdf36
Youtirsin <youtirsin@qq.com>
2023-12-12 12:19:49 +0800

    log

--- tests/log.cpp
+++ tests/log.cpp
@@ -1,24 +1,26 @@
 #include "log.hpp"
 
 int main (int, char**) {
+  auto log = log::get();
+
   const int x = 1;
-  log::info("hello world ", x);
-  log::error("hello world ", x);
-  log::infoln("hello world ", x);
-  log::errorln("hello world ", x);
+  log->info("hello world ", x);
+  log->error("hello world ", x);
+  log->infoln("hello world ", x);
+  log->errorln("hello world ", x);
 
-  log::on(false);
-  log::info("hello world ", x);
-  log::error("hello world ", x);
-  log::infoln("hello world ", x);
-  log::errorln("hello world ", x);
+  log->on(false);
+  log->info("hello world ", x);
+  log->error("hello world ", x);
+  log->infoln("hello world ", x);
+  log->errorln("hello world ", x);
 
-  log::on(true);
-  log::with_log_file("./test.log");
-  log::info("hello world ", x);
-  log::error("hello world ", x);
-  log::infoln("hello world ", x);
-  log::errorln("hello world ", x);
+  log->on(true);
+  log->with_log_file("./test.log");
+  log->info("hello world ", x);
+  log->error("hello world ", x);
+  log->infoln("hello world ", x);
+  log->errorln("hello world ", x);
   
   return 0;
 }
